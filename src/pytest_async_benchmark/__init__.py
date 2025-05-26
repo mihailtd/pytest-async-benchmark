a741cc97bc0b71dc6fa7c144e047245a6ae122bb
Mihai Farcas <farcasmihai91@gmail.com>
2025-05-27 02:09:58 +0300

    fix: bump version to 0.1.1

--- src/pytest_async_benchmark/__init__.py
+++ src/pytest_async_benchmark/__init__.py
@@ -1,6 +1,6 @@
 """pytest-async-benchmark: Modern pytest benchmarking for async code. 🚀"""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Mihai Farcas"
 __email__ = "contact@mihai.ltd"
 
